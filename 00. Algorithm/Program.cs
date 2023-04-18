a8ecc28d0ffde70667e24ee06986965f5fd43238
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-18 15:49:13 +0900

    Initial commit

---
+++ 00. Algorithm/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace _00._Algorithm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp_Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
