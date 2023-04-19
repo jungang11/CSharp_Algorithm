6d98de355fb5ca3550ae7521a3cf0baaa933a10e
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-19 18:11:58 +0900

    feat : LinkedList
    LinkedList 및 문서 수정
    AddFirst() AddLast() 구현

---
+++ 02. LinkedList/LinkedList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedListNode<T> // LinkedList는 Node 기반이기 때문에 LinkedListNode 구현
+    {
+        internal LinkedList<T> list;         // 소속 리스트
+        internal LinkedListNode<T> prev;     // 이전 위치
+        internal LinkedListNode<T> next;     // 다음 위치
+        private T item;                      // 실제 데이터
+
+        // 생성자 3종류
+        public LinkedListNode(T value)
+        {
+            this.list = null;
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, T value)
+        {
+            this.list = list;
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, LinkedListNode<T> prev, LinkedListNode<T> next, T value)
+        {
+            this.list = list;
+            this.prev = prev;
+            this.next = next;
+            this.item = value;
+        }
+
+        // property
+        public LinkedList<T> List { get { return list; } }
+        public LinkedListNode<T> Prev { get { return prev; } }
+        public LinkedListNode<T> Next { get { return next; } }
+        // value 는 읽기/쓰기 가능
+        public T Value { get { return item; } set { item = value; } }
+    }
+    
+    public class LinkedList<T>
+    {
+        // head : 가장 앞에 있는 노드 / tail : 가장 뒤에 있는 노드
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+        private int count;  // 몇 개 가지고 있는지
+
+        // 생성자
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        // property
+        public LinkedListNode<T> First { get { return head; } }
+        public LinkedListNode<T> Last { get { return tail; } }
+        public int Count { get { return count; } }
+
+        // AddFirst 가장 앞에 붙이기
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            // 1. 새로운 노드 만들기
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            // 2. 연결 구조 바꾸기 -> 클래스이므로 주소의 참조
+            if(head != null)        // 2-1. head 노드가 있을 때
+            {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            else                    // 2-2. head 노드가 없을 때 -> newNode밖에 없으니 head, tail이 newNode
+            {
+                head = newNode;
+                tail = newNode;
+            }
+
+            // 3. 갯수 늘리기
+            count++;
+            return newNode;
+        }
+
+        public LinkedListNode<T> AddLast(T value)
+        {
+            // 1. 새로운 노드 만들기
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            // 2. 연결 구조 바꾸기 -> 클래스이므로 주소의 참조
+            if(tail != null)
+            {
+                newNode.prev = tail;
+                tail.next = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                head = newNode;
+                tail = newNode;
+            }
+
+            // 3. 갯수 늘리기
+            count++;
+            return newNode;
+        }
+
+        // LinkedList 기술면접 조사 - List LinkedList 차이점
+
+    }
+}
---
+++ 02. LinkedList/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace _02._LinkedList
+{
+    internal class Program
+    {
+        /******************************************************
+		 * 연결리스트 (Linked List)
+		 * 
+		 * 데이터를 포함하는 노드들을 연결식으로 만든 자료구조
+		 * 노드는 데이터와 이전/다음 노드 객체를 참조하고 있음
+		 * 노드가 메모리에 연속적으로 배치되지 않고 이전/다음노드의 위치를 확인
+		 * 
+		 * 힙 영역의 데이터들이 연속적으로 있지 않지만 이전/다음 데이터의 위치를 알고있음
+		 * 배열처럼 데이터들이 연속적이지 않기 때문에 배열의 경우보다 추가/삭제가 효율적임
+		 * 하나씩 당겨오거나 할 필요없이 가리키는 참조 위치만 바꾸어주면 되기 때문
+		 ******************************************************/
+
+        // <링크드리스트 사용>
+        // 추가/삭제가 용이해서 AddFirst, AddLast의 함수가 제공됨
+        void LinkedList()
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+            // 링크드리스트 요소 삽입
+            linkedList.AddFirst("0번 앞데이터");
+            linkedList.AddFirst("1번 앞데이터");
+            linkedList.AddLast("0번 뒤데이터");
+            linkedList.AddLast("1번 뒤데이터");
+
+            // 링크드리스트 요소 삭제 => O(n) 
+            linkedList.Remove("1번 앞데이터");
+
+            // 링크드리스트 요소 탐색
+            LinkedListNode<string> findNode = linkedList.Find("0번 뒤데이터");
+
+            // 링크드리스트 노드를 통한 노드 참조
+            // 링크드리스트는 노드 기반의 연결 구조
+            LinkedListNode<string> prevNode = findNode.Previous;
+            LinkedListNode<string> nextNode = findNode.Next;
+
+            // 링크드리스트 노드를 통한 노드 삽입
+            linkedList.AddBefore(findNode, "찾은노드 앞데이터");
+            linkedList.AddAfter(findNode, "찾은노드 뒤데이터");
+
+            // 링크드리스트 노드를 통한 삭제 => O(1)
+            linkedList.Remove(findNode);
+        }
+
+        // <List의 시간복잡도>     => LinkedList와 접근의 시간복잡도가 다른 이유는 데이터의 연속성
+        // 접근		탐색		삽입		삭제
+        // O(1)		O(n)	    O(n)	    O(n)
+
+        // <LinkedList의 시간복잡도> => 대신 연속적이지 않기 때문에 삽입/삭제가 효율적임
+        // 접근		탐색		삽입		삭제
+        // O(n)		O(n)	    O(1)	    O(1)
+
+        // 삽입/삭제 효율이 좋고 상대적으로 접근/탐색의 효율이 좋지않음 => 연속적이지 않기 때문
+        // 특정 노드를 찾기 위해선 하나씩 찾아봐야함
+        // LinkedList는 배열의 구조가 아니기 때문에 index를 사용할 수 없음
+        // 연결형리스트는 삽입/삭제의 속도가 중요한 자료에 쓸 수 있음
+        // 배열처럼 데이터가 꽉 차있을 경우가 없기 때문에 언제든 추가할 수 있음
+
+        // C# 은 가비지컬렉터의 유무 때문에 LinkedList를 많이 사용하진 않음 (c++에선 유용)
+        // => 이진탐색같은 노드기반의 구조 또한 마찬가지
+
+        // 단방향 LinkedList : 이전의 주소가 필요없음 -> 참조용 데이터가 하나 적음
+        // 양방향 LinkedList : 이전, 다음의 참조용 데이터가 필요
+        // 환 형 LinkedList : 양방향과 비슷하지만 마지막 노드는 처음의 노드를 가리킴 -> c#
+
+        public static void Main(string[] args)
+        {
+            DataStructure.LinkedList<int> linkedList = new DataStructure.LinkedList<int>();
+
+            linkedList.AddFirst(0);
+            linkedList.AddLast(1);
+            linkedList.AddFirst(2);
+            linkedList.AddLast(3);
+            linkedList.AddFirst(4);
+            linkedList.AddLast(5);
+        }
+    }
+}
