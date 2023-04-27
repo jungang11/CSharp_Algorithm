b28c9291be2dc9a6747e48a0da9cbcb25bb34623
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-27 21:43:06 +0900

    feat : Hanoi
    시간초과
    -> StringBuilder 수정필요

---
+++ 09. DesignTechnique/BackJoon/Hanoi.cs
@@ -0,0 +1,31 @@
+﻿namespace _09._DesignTechnique.BackJoon
+{
+    // 시간초과
+
+    public class Hanoi
+    {
+        public static void Move(int count, int start, int end)
+        {
+            int other = 6 - start - end;
+
+            if(count == 1)
+            {
+                Console.WriteLine($"{start} {end}");
+            }
+            else
+            {
+                Move(count - 1, start, other);
+                Console.WriteLine($"{start} {end}");
+                Move(count - 1, other, end);
+            }
+        }
+        
+        public static void hanoi()
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(Math.Pow(2, n) - 1);
+            Move(n, 1, 3);
+        }
+    }
+}
