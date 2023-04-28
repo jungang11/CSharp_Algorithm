2cc12feea2aac7d0503bab3fb42f7a5ce7bca8a9
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-28 12:04:38 +0900

    feat : 하노이 시간초과 수정
    Console.Write 대신
    stringbuilder sb에 저장한 후 출력하니 시간초과 해결됐음

--- 09. DesignTechnique/BackJoon/Hanoi.cs
+++ 09. DesignTechnique/BackJoon/Hanoi.cs
@@ -1,31 +1,37 @@
-﻿namespace _09._DesignTechnique.BackJoon
+﻿using System.Text;
+
+namespace _09._DesignTechnique.BackJoon
 {
     // 시간초과
 
     public class Hanoi
     {
+        static StringBuilder sb = new StringBuilder();
+
         public static void Move(int count, int start, int end)
         {
             int other = 6 - start - end;
 
             if(count == 1)
             {
-                Console.WriteLine($"{start} {end}");
+                sb.AppendFormat($"{start} {end}\n");
+                return;
             }
             else
             {
                 Move(count - 1, start, other);
-                Console.WriteLine($"{start} {end}");
+                sb.AppendFormat($"{start} {end}\n");
                 Move(count - 1, other, end);
             }
         }
         
-        public static void hanoi()
+        public static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
             
             Console.WriteLine(Math.Pow(2, n) - 1);
             Move(n, 1, 3);
+            Console.WriteLine(sb);
         }
     }
 }
