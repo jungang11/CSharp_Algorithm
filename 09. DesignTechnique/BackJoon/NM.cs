dc3e2d2073ae6318f831039a07d453bfd2f78872
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-28 12:19:24 +0900

    fix : N과M(3)수정 오류해결
    출력은 stringbuilder 이용
    한줄에 두개 입력받기 위해 split

--- 09. DesignTechnique/BackJoon/NM.cs
+++ 09. DesignTechnique/BackJoon/NM.cs
@@ -9,19 +9,25 @@ namespace _09._DesignTechnique.BackJoon
 {
     internal class NM
     {
+        // 출력위해 sb에 저장
+        static StringBuilder sb = new StringBuilder();
+
+        // 재귀함수
         public static void Back(int[] arr, int index, int N, int M)
         {
-            // 횟수가 M이 되면 배열 출력
+            // index가 M이 되면 배열 출력
             if (index == M)
             {
                 for(int i = 0; i < M; i++)
                 {
-                    Console.Write($"{arr[i]} ");
+                    sb.AppendFormat("{0} ", arr[i]);
                 }
-                Console.WriteLine();
+                sb.Append("\n");
+                return;
             }
-            for (int i = 1; i < N+1; i++)
+            for (int i = 1; i <= N; i++)
             {
+                // 재귀
                 arr[index] = i;
                 Back(arr, index + 1, N, M);
             }
@@ -30,15 +36,15 @@ namespace _09._DesignTechnique.BackJoon
         // M개까지 N줄 출력
         public static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
-            int M = int.Parse(Console.ReadLine());
-            int[] arr = new int[N];
-            for(int i = 0; i < N; i++)
-            {
-                arr[i] = i+1;
-            }
+            // 한 줄에 두개 입력받기 위해 s 문자열을 공백기준으로 나눔
+            string[] s = Console.ReadLine().Split();
+            // s 문자열 나눈대로 N과 M
+            int N = int.Parse(s[0]);
+            int M = int.Parse(s[1]);
+            int[] arr = new int[N+1];
 
             Back(arr, 0, N, M);
+            Console.WriteLine(sb);
         }
     }
 }
