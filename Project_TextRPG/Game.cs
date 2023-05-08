0f6889039b8f1e36ec81e685e3a6619e33186996
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-05-08 11:34:33 +0900

    feat : textGame init

---
+++ Project_TextRPG/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TextRPG
+{
+    internal class Game
+    {
+        public void Run()
+        {
+            // 초기화
+            Init();
+
+            // 게임 루프
+            while(true)
+            {
+                // 렌더링
+                Render();
+                // 입력
+                // 갱신
+                Update();
+            }
+            // 마무리
+            Release();
+        }
+
+        private void Init()
+        {
+
+        }
+
+        private void Render()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void Release()
+        {
+
+        }
+    }
+}
